9e80eb919d8230aaa97ed62455e314a7d25088f1
GustavoSZ124 <gustavosz1224@gmail.com>
2020-11-28 12:34:37 -0600

    Componente Navbar ngModel

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,19 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  isLogged:boolean = false;
-  num:number = 0;
+  isLogged: boolean = false;
+  num: number;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  increment(): void {
+    if (this.num == null) this.num = 0;
+    this.num++;
   }
 
-  increment(){
-    this.num++;
+  random(): void {
+    let max = 200;
+    let min = 50;
+    this.num = Math.round(Math.random() * (max + 1 - min) + min);
   }
 }
